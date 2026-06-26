8fcd82ed1dcde072481e5f249d5debf659207035
Vardaan Saxena <100420014+SRV-KILLER09@users.noreply.github.com>
2026-06-26 12:12:49 +0530

    Update AccountManager.h

--- include/AccountManager.h
+++ include/AccountManager.h
@@ -1,13 +1,10 @@
 #ifndef ACCOUNTMANAGER_H
 #define ACCOUNTMANAGER_H
-
 #include "User.h"
-
 class AccountManager
 {
 public:
     bool login(User user);
     void changePassword(User &user);
 };
-
 #endif
